2999b7a78fe7e2b006c38f36549b726ccd85d0b4
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2025-02-04 10:04:22 +0100

    Tank controls!

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,39 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
   Animator animator;
+  CharacterController characterController;
   bool walking = false;
   Vector2 moveInput = Vector2.zero;
   [SerializeField]
   float speed = 5;
 
+  [SerializeField]
+  float rotationSpeed = 180;
+
   void Start()
   {
     animator = GetComponent<Animator>();
+    characterController = GetComponent<CharacterController>();
   }
 
   void Update()
   {
-    transform.Translate(moveInput * speed * Time.deltaTime);
+    transform.Rotate(Vector3.up, moveInput.x * rotationSpeed * Time.deltaTime);
+
+    Vector3 movement = transform.forward * moveInput.y;
+
+    if (movement.magnitude > 0)
+    {
+      animator.SetBool("moving", true);
+    }
+    else
+    {
+      animator.SetBool("moving", false);
+    }
+
+    characterController.Move(movement * speed * Time.deltaTime);
+
+    // transform.Translate(moveInput * speed * Time.deltaTime);
   }
 
   void OnMove(InputValue value)
